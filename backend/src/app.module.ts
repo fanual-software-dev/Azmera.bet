cec6a15de56e20366021634a96f41252946224be
fanual-software-dev <asfawfanual2003@gmail.com>
2025-10-08 21:02:02 +0300

    feat: Auth guard

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,8 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { SmsModule } from './sms/sms.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [
@@ -16,6 +18,12 @@ import { SmsModule } from './sms/sms.module';
     SmsModule, // from .env
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard
+    }
+  ],
 })
 export class AppModule {}
